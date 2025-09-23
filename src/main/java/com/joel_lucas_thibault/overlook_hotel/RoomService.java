67b21117c6a8ddfda06758b09ad52c77332d7f98
thibault-manse <thibault.manse@laplateforme.io>
2025-09-23 10:17:41 +0200

    update : english traduction

---
+++ src/main/java/com/joel_lucas_thibault/overlook_hotel/RoomService.java
@@ -0,0 +1,19 @@
+package com.joel_lucas_thibault.overlook_hotel;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+}
